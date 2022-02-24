3e42d766223ead1fecc68288d509a1cb5750007a
Emily Roth <errskipower@gmail.com>
2022-02-24 08:55:42 -0500

    adding header and footer to index

--- theme/cheersto2022/functions.php
+++ theme/cheersto2022/functions.php
@@ -3,8 +3,14 @@
 if ( ! function_exists( 'emptytheme_support' ) ) :
 	function emptytheme_support()  {
 
+		// Add default posts and comments RSS feed links to <head>.
+    add_theme_support( 'automatic-feed-links' );
+
 		// Adding support for core block visual styles.
 		add_theme_support( 'wp-block-styles' );
+		// Enable support for post thumbnails and featured images.
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'editor-styles' );
 
 		// Enqueue editor styles.
 		add_editor_style( 'style.css' );
